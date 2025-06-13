3e3a527048d60c42633a504faf42db064f67fa5d
Ratnesh Kumar <ratnesh134@gmail.com>
2025-06-13 12:46:44 +0530

    Day 2

---
+++ string_operations.py
@@ -0,0 +1,45 @@
+# We will learn about Strings in this section
+
+string = input("Enter Any String of your choice : ")
+
+
+# Operations Performed
+
+# Indexing
+
+# Positive Indexing
+
+print(string[0])
+
+# Negative Indexing
+
+prnit(string[-2])
+
+
+# Slicing 
+
+print(string[0:4])
+
+# Reverse the string
+
+print(string[::-1])
+
+
+# Edit the string ---> Strings are Immutable data type
+
+# String Functions
+
+print(len(string))
+
+# Maximum value in the string 
+
+print(max(string))
+
+# Soting the string---> output is in the form of list
+
+print(sorted(string))
+
+print(sorted(string,reverse=True)) # Output will be in reverse orde
+  
+
+ 
