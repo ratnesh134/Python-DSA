a33fccb1995bcfa55724b7cb15a035d1cc067918
Ratnesh Kumar <ratnesh134@gmail.com>
2025-06-21 14:01:50 +0530

    Function Basic

---
+++ even_odd.py
@@ -0,0 +1,13 @@
+def is_even_odd(num):
+    '''
+    Here we are writing a function to check whether the 
+    given number is even or odd
+    '''
+    if num%2 == 0:
+        print("Number is even")
+    else:
+        print("Number is odd")
+
+
+is_even_odd(int(input("Enter the number of your choice : ")))
+
