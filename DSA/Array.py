b13bc6b67275accb4e16b053150497ba2540d36e
Ratnesh Kumar <ratnesh134@gmail.com>
2026-01-16 16:21:30 +0530

    dynamic approach

--- DSA/Array.py
+++ DSA/Array.py
@@ -1,5 +1,15 @@
-import array
+from array import *
 
-val = array.array("i",[1,2,3,4,5,6])
+val = array("i",[1,2,3,4,5,6])
 
-print(val)
+for i in val:
+    print(i, end=" ")
+
+
+print("\n")
+# Better Approach
+
+for i in range(0,len(val)):
+    print(val[i],end=" ")
+
+print("\n")
