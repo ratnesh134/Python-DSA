7ca0ac328b71e3a18d2c1e6f986252841b7bbd2d
Ratnesh Kumar <ratnesh134@gmail.com>
2026-01-19 21:03:23 +0530

    Arange function

--- DSA/numpy_op.py
+++ DSA/numpy_op.py
@@ -15,3 +15,12 @@ for x in val:
     print(x)
 
 print("\n")
+
+# Arange
+
+val = np.arange(10,20,2) # arange(start, end, space) Does not include the end.
+
+for x in val:
+    print(x)
+
+print("\n")
