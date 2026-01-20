8601d0977580c22c5e84c745ad8999513486e594
Ratnesh Kumar <ratnesh134@gmail.com>
2026-01-20 22:51:41 +0530

    SLL constructor

---
+++ DSA/SinglyLL.py
@@ -0,0 +1,15 @@
+class Node:
+
+    # Constructor
+    def __init__(self,info,next=None):
+        self.data = info
+        self.next = next
+
+
+class SinglyLinkedList:
+
+    # Constructor
+    def __init__(self,head=None):
+        self.head = head
+    
+    
