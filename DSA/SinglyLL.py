bb5f73f9d869ad13def09d4c71cdf9248137752a
Ratnesh Kumar <ratnesh134@gmail.com>
2026-01-21 12:45:09 +0530

    End Insertion

--- DSA/SinglyLL.py
+++ DSA/SinglyLL.py
@@ -12,4 +12,19 @@ class SinglyLinkedList:
     def __init__(self,head=None):
         self.head = head
     
-    
+    def insertAtEnd(self,value):
+        temp = Node(30)
+
+        # When Linked List is already created
+        if (self.head != None):
+            t1 = self.head
+
+            while (t1.next != None):
+                t1 = t1.next
+            t1.next = temp
+
+        # When the Linked List is not created
+        else:
+            self.head = temp
+        
+
