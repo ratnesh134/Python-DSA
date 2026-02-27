4ac8bca39bea3c45b58e0c2d877045071f650540
Ratnesh Kumar <ratnesh134@gmail.com>
2026-02-27 21:44:27 +0530

    Delete element

--- DSA/stack.py
+++ DSA/stack.py
@@ -12,12 +12,25 @@ class Stack:
     def push(self,value):
         self.ls.insert(0,value)
 
-    # to see the latest element
+    # to see the latest element 
     def peek(self):
         if len(self.ls) == 0:
             raise Exception("Empty Stack/List")
         else:
             return self.ls[0]
+    
+    # delete element
+    def pop(self):
+        if len(self.ls) == 0:
+            raise Exception("Empty Stack/List")
+        else:
+            return self.ls.pop(0) # Need to specify the index since we are using the insert function instead of append
+        
+
+    
+        
+        
+    
     
 
 
