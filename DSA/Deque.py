24fc32fcf255c7b0234967d931a3df13cd9d05f4
Ratnesh Kumar <ratnesh134@gmail.com>
2026-03-09 21:18:03 +0530

    deletion end with check

--- DSA/Deque.py
+++ DSA/Deque.py
@@ -21,5 +21,15 @@ class Deque:
         self.items.insert(0,value)
     
     def deleteAtRear(self):
-        self.items.pop()
+        if self.isEmpty():
+            print("Queue is empty")
+        else:
+            return self.items.pop()
     
+d = Deque()
+d.insertAtEnd(10)
+d.insertAtEnd(30)
+d.insertAtEnd(20)
+d.insertAtBeg(100)
+d.insertAtEnd(40)
+d.insertAtBeg(50)
