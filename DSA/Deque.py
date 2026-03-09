c325ea2cd9919af02dbd0c12092981f2cff681c9
Ratnesh Kumar <ratnesh134@gmail.com>
2026-03-09 21:09:57 +0530

    Insert at front

---
+++ DSA/Deque.py
@@ -0,0 +1,18 @@
+class Deque:
+
+    def __init__(self):
+        self.items = []
+
+    
+    def isEmpty(self):
+        return len(self.items) == 0
+    
+    def insertAtEnd(self,value):
+        return self.items.append(value)
+    
+    def deleteAtFront(self):
+
+        if self.isEmpty():
+            print("Queue is empty")
+        else:
+            return self.items.pop(0)
