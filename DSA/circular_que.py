c011bd32af2dad42ef11bc190742794b7d95098f
Ratnesh Kumar <ratnesh134@gmail.com>
2026-03-09 22:32:33 +0530

    Insertion element rear

--- DSA/circular_que.py
+++ DSA/circular_que.py
@@ -4,4 +4,18 @@ class CircularQueue:
         self.items = [None]*size #makes a list of size (N) which has default value as None
         self.front = self.rear = -1
 
+    # Insertion of element
+    def enqueue(self,value):
+        if ((self.rear + 1) % self.size == self.front):
+            print("Queue is full")
+        
+        # empty queue
+        elif self.front == -1:
+            self.front = self.rear = 0
+            self.items[self.rear] = value
+        
+        else:
+            self.rear = (self.rear + 1)% self.size
+            self.items[self.rear] = value
+        
     
