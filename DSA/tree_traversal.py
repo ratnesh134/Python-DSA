410f8c5b04ac38fd74f55e0150499e471bdb2d91
Ratnesh Kumar <ratnesh134@gmail.com>
2026-03-19 22:10:42 +0530

    Preorder Traversal Function

--- DSA/tree_traversal.py
+++ DSA/tree_traversal.py
@@ -6,3 +6,19 @@ class Node  :
         self.right = None
         self.data = value
     
+# Pre Order Traversal Logic
+def preorder(root):
+    if (root != None):
+        print(root.data,end=" ")
+    preorder(root.left)
+    preorder(root.right)
+
+
+
+root = Node(1)
+root.left = Node(3)
+root.right = Node(5)
+root.left.left = Node(2)
+root.left.right = Node(4)
+root.right.right = Node(8)
+
