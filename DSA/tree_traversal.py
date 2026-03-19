c5c7a3649c37ea4d6b5e4fa018061419df115d5a
Ratnesh Kumar <ratnesh134@gmail.com>
2026-03-19 22:19:04 +0530

    preOrder func fix

--- DSA/tree_traversal.py
+++ DSA/tree_traversal.py
@@ -6,12 +6,12 @@ class Node  :
         self.right = None
         self.data = value
     
-# Pre Order Traversal Logic
+# Pre Order Traversal Logic 
 def preorder(root):
     if (root != None):
         print(root.data,end=" ")
-    preorder(root.left)
-    preorder(root.right)
+        preorder(root.left)
+        preorder(root.right)
 
 
 
@@ -22,3 +22,5 @@ root.left.left = Node(2)
 root.left.right = Node(4)
 root.right.right = Node(8)
 
+preorder(root)
+print("\n")
