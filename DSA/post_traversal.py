7fd6d24f22a1c1f10218b5a0f14aabcf44d3ae1a
Ratnesh Kumar <ratnesh134@gmail.com>
2026-03-19 22:26:40 +0530

    PostOrder Traversal

---
+++ DSA/post_traversal.py
@@ -0,0 +1,27 @@
+# Node Creation
+class Node  :
+
+    def __init__(self, value):
+        self.left = None
+        self.right = None
+        self.data = value
+    
+# Pre Order Traversal Logic 
+def postOrder(root):
+    if (root.left != None):
+        
+        postOrder(root.right)
+        postOrder(root)
+        print(root.data,end=" ")
+
+
+
+root = Node(1)
+root.left = Node(3)
+root.right = Node(5)
+root.left.left = Node(2)
+root.left.right = Node(4)
+root.right.right = Node(8)
+
+postOrder(root)
+print("\n")
